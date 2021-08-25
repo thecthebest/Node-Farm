d73a79458552dc122d4f55ae4c808bb1bbf39fd9
alis <asyed5839@gmail.com>
2021-08-25 19:34:36 +0100

    Storing files to variables
    The variables called "tempOverview", "tempCard" & "tempProduct" are used
    to store html files.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http = require("http");
 const fs = require("fs");
 
+const tempOverview = fs.readFileSync(`${__dirname}/overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/template-product.html`, 'utf-8');
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
