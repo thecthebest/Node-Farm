b28ae951bb08b83413733efb2f6a675a363b088c
alis <asyed5839@gmail.com>
2021-08-25 19:11:28 +0100

    Added logic for reading url
    "pathName" stores the result from the URL and then check that value
    against "/" or "/overview" in the first "if" block. if the result is
    true then a response is sent to the client otherwise checked is moved to
    the next "else if" statement. Finally if both "if" checks fails then a
    "404" response is sent.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  const pathName = req.url;
 
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else {
+    res.writeHead(404, {
+        'Content-type':'text/html'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(8080);
