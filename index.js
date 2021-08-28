55bc1fc4fb3db401fdd09335fe43d31f1f305ade
alis <asyed5839@gmail.com>
2021-08-28 19:08:58 +0100

    Generating html template using JSON
    Html template is generated running a ".map()" method which calls
    "replaceTemplate" function. The function takes 2 arguments "tempCard" and "ele", these arguments are passed inside the function and then the function replaces place holders with the current value of "ele". Inside the function there is "output" variables which appends the old "output" to the new "output".Finally the function returns "output" and a "join()" is run to combine all the values returned.

--- index.js
+++ index.js
@@ -8,11 +8,26 @@ const tempProduct = fs.readFileSync(`${__dirname}/template-product.html`, 'utf-8
 const data = fs.readFileSync(`${__dirname}/data.json`);
 const dataObj = JSON.parse(data);
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%img%}/g, product.image);
+  output = output.replace(/{%qunatity%}/g, product.qunatity);
+  output = output.replace(/{%productName%}/g, product.productName);
+  output = output.replace(/{%price%}/g, product.price);
+  output = output.replace(/{%id%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%not_organic%}/g, 'not-organic');
+  }
+  return output;
+};
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === "/" || pathName === "/overview") {
     res.writeHead(200, {'Content-type':'text/html'});
+    const cardsHtml = dataObj
+      .map(ele => replaceTemplate(tempCard, ele))
+      .join('');
     res.end(tempOverview);
   } else if (pathName === "/product") {
     res.end("This is the product");
